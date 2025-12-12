fb9b2c3f4a665d360daacd9dc8f1141f655938ce
Lê Văn Luận <22130156@st.hcmuaf.edu.vn>
2025-12-12 23:39:28 +0700

    form contact

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,47 +1,87 @@
 import React from "react";
+import "../styles/styles.css";
+import IconScroll from "../components/icon-scroll";
 
 function Contact() {
     return (
-        <section className="contact-section">
-            <div className="contact-container">
-                <div className="dashed-line"></div>
+        <>
+            <IconScroll />
 
-                <div className="contact-grid">
-                    <div className="contact-box">
-                        <div className="icon-wrapper"><i className="fa-solid fa-location-dot"></i></div>
+            <div className="container-contact">
+                <div className="header-contact">
+                    <h1 className="title-contact">LIÊN HỆ</h1>
+                    <p className="subtitle-contact">
+                        Thông tin liên hệ & hỗ trợ khách hàng
+                    </p>
+                </div>
+
+                <div className="contact-content">
+                    {/* Địa chỉ */}
+                    <div className="contact-row">
+                        <div className="contact-icon"><i className="fa-solid fa-location-dot"></i></div>
                         <div>
-                        <div className="title">Địa chỉ</div>
-                            <div className="text">
-                                Khu phố 33, phường Linh Xuân<br /> TP. HCM
+                        <div className="contact-title">Địa chỉ</div>
+                            <div className="contact-text">
+                                Khu phố 33, phường Linh Xuân, TP.HCM
                             </div>
                         </div>
                     </div>
 
-                    <div className="contact-box">
-                        <div className="icon-wrapper"><i className="fa-solid fa-phone"></i></div>
+                    {/* Hotline */}
+                    <div className="contact-row">
+                        <div className="contact-icon"><i className="fa-solid fa-phone"></i></div>
                         <div>
-                        <div className="title">Hotline</div>
-                            <div className="text">
-                                0868 338 302 <span>(Đặt cơm)</span><br />
-                                <span>(Hợp tác)</span>
+                        <div className="contact-title">Hotline</div>
+                            <div className="contact-text">
+                                0971649429 - 0383107754
                             </div>
                         </div>
                     </div>
 
-                    <div className="contact-box">
-                        <div className="icon-wrapper"><i className="fa-solid fa-envelope"></i></div>
+                    {/* Email */}
+                    <div className="contact-row">
+                        <div className="contact-icon"><i className="fa-solid fa-envelope"></i></div>
                         <div>
-                        <div className="title">Email</div>
-                            <div className="text">
-                                yennhinguyen102@gmail.com<br />
-                                <span>(Trao đổi hợp tác)</span>
+                        <div className="contact-title">Email</div>
+                            <div className="contact-text">
+                                22130156@st.hcmuaf.edu.vn
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="dashed-line"></div>
+
+                <div className="contact-form-box">
+                    <h2 className="form-title">Thông tin thắc mắc, quý khách vui lòng liên hệ tại đây:</h2>
+
+                    <form className="contact-form">
+
+                        <label>Tên quý khách</label>
+                        <input type="text" placeholder="Tên quý khách" />
+
+                        <div className="form-row">
+                            <div className="form-group">
+                                <label>Số điện thoại (*)</label>
+                                <input type="text" placeholder="Số điện thoại" />
+                            </div>
+
+                            <div className="form-group">
+                                <label>Email</label>
+                                <input type="text" placeholder="Email" />
+                            </div>
+                        </div>
+
+                        <label>Hệ thống chi nhánh</label>
+                        <input type="text" placeholder="Hệ thống chi nhánh" />
+
+                        <label>Nội dung thắc mắc của quý khách</label>
+                        <textarea placeholder="Nội dung thắc mắc của quý khách"></textarea>
+
+                        <button type="submit" className="btn-submit">Gửi thông tin</button>
+                    </form>
+                </div>
+
             </div>
-        </section>
+        </>
     );
 }
 
