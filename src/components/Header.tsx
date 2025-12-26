43a44c778e6f828829bb4fadf8e4c9969ca54c85
Lê Văn Luận <22130156@st.hcmuaf.edu.vn>
2025-12-26 23:19:42 +0700

    create CartContext

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,15 @@ import React, { useEffect, useState } from "react";
 import { NavLink, useNavigate, useLocation } from "react-router-dom";
 import "../styles/styles.css";
 import {api} from "../services/api";
+import {useContext} from "react";
+import {CartContext} from "./CartContext";
 
 const Header = () => {
     const [user, setUser] = useState<{ username: string } | null>(null);
     const [keyword, setKeyword] = useState("");
     const navigate = useNavigate();
     const location = useLocation();
+    const {totalQuantity} = useContext(CartContext);
 
     useEffect(() => {
         const storedUser = localStorage.getItem("user");
@@ -119,6 +122,7 @@ const Header = () => {
                         {/*{cartCount > 0 && (*/}
                         {/*    <span className="cart-badge">{cartCount}</span>*/}
                         {/*)}*/}
+                        {totalQuantity > 0 && <span className="cart-badge">{totalQuantity}</span>}
                     </NavLink>
 
 
--- src/components/item-menu.tsx
+++ src/components/item-menu.tsx
@@ -1,29 +1,37 @@
 import React from "react";
-import { Product } from "../types/object";
+import {Product} from "../types/object";
 import "../styles/styles.css";
 import {NavLink} from "react-router-dom";
 import {formatPrice} from "./formatPrice";
+import {useContext} from "react";
+import {CartContext} from "./CartContext";
+
 
 interface ItemMenuProps {
     product: Product;
 }
 
-function ItemMenu({ product }: ItemMenuProps) {
+function ItemMenu({product}: ItemMenuProps) {
+    const { addToCart } = useContext(CartContext);
     return (
         <div className={"item-menu"}>
-           <NavLink to={`/product/${product.id}`}>
-               <img
-                   className={"imgItem-menu"}
-                   src={product.img}
-                   alt={product.name}
-               />
-           </NavLink>
+            <NavLink to={`/product/${product.id}`}>
+                <img
+                    className={"imgItem-menu"}
+                    src={product.img}
+                    alt={product.name}
+                />
+            </NavLink>
             <div className={"title-item-menu"}>{product.name}</div>
             <div className={"flex-row"}>
                 <div className={"price-menu"}>
-                    {formatPrice(product?.price??0)}
+                    {formatPrice(product?.price ?? 0)}
                 </div>
-                <button className={"add-cart"}>Thêm vào giỏ</button>
+                <button className={"add-cart"}
+                        onClick={() => addToCart(product)}
+                >
+                    Thêm vào giỏ
+                </button>
             </div>
         </div>
     );
